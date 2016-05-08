e49d5ff8c3a526d005186a91634aa33112ed2993
justin yu <justin yu>
2016-05-08 01:24:17 -0700

    Edited Main/Ramen

---
+++ js/Main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){ // start js once document loads
+//creating a firebase reference 
+var ref = new Firebase("https://DormHacks.firebaseio.com");
+$("#submitItems").click(function(){
+   var items = $("#items").val();
+   //login will recieve email and password from user
+console.log(items);
+   var url_ramen = "https://dormhacks.firebaseio.com/user_ramen";
+   new Firebase(url_ramen).once('value',function(snap){
+      var ramen = snap.val().ramen; 
+      i=ramen.length;
+      ramen[i]=items;
+      console.log(ramen);
+      ref.child("user_ramen").update({
+         ramen
+  
+      });
+      
+   });
+});
+$("#menu1").click(function(){
+   var items = $("#items").val();
+   //login will recieve email and password from user
+console.log(items);
+   var url_ramen = "https://dormhacks.firebaseio.com/user_ramen";
+   new Firebase(url_ramen).once('value',function(snap){
+      var ramen = snap.val().ramen; 
+      console.log(ramen);
+      localStorage["ramen_test1"] = JSON.stringify(ramen);
+   });
+
+   }
+   );
+});
---
+++ js/Ramen.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){ // start js once document loads
+var ramen = JSON.parse(localStorage["ramen_test1"]);
+   console.log(ramen_test1);
+});
